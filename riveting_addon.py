3827ab6c665a4046b38db46c130fb1936b5d2058
Vladislav Brusokas <brusvlad4002@yandex.ru>
2025-07-27 21:17:26 +0100

    Integrated bmesh module with the UI - Now, pressing the button applies bevel to the object selected - There is a slider that is accessible after the operation is applied

--- riveting_addon.py
+++ riveting_addon.py
@@ -9,6 +9,40 @@ bl_info = {
 }
 
 import bpy
+import bmesh
+
+class MESH_OT_add_rivets(bpy.types.Operator):
+    bl_idname = "mesh.add_rivets"
+    bl_label = "Add Rivets to the Mesh"
+    bl_options = {"REGISTER", "UNDO"}
+    
+    value: bpy.props.FloatProperty(
+        name="Value",
+        default=0.5,
+        description="The editable value",
+    )
+    
+    def execute(self, context):
+        
+        mesh_obj = bpy.context.active_object
+        
+        bm = bmesh.new()
+        
+        bm.from_mesh(mesh_obj.data)
+        
+        bmesh.ops.bevel(bm, geom=bm.verts, offset=self.value)
+        
+        bm.normal_update()
+        
+        bm.to_mesh(mesh_obj.data)
+        
+        mesh_obj.data.update()
+        
+        bm.free()
+        
+        #bpy.ops.mesh.primitive_monkey_add(size=self.mesh_size)
+        
+        return {"FINISHED"}
 
 class VIEW3D_PT_riveting_panel(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
@@ -19,14 +53,16 @@ class VIEW3D_PT_riveting_panel(bpy.types.Panel):
     
     def draw(self, context):
         row = self.layout.row()
-        row.operator("mesh.primitive_cube_add", text="Placeholder Op")
+        row.operator("mesh.add_rivets", text="Add Rivets")
         
 def register():
     bpy.utils.register_class(VIEW3D_PT_riveting_panel)
+    bpy.utils.register_class(MESH_OT_add_rivets)
     
     
 def unregister():
     bpy.utils.unregister_class(VIEW3D_PT_riveting_panel)
+    bpy.utils.unregister_class(MESH_OT_add_rivets)
     
     
 if __name__ == "__main__":
